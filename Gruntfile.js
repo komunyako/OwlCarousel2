ea56d7d62e9bb503ae05cc32e8e5cae90f792d48
Komunyako <max.kanda@gmail.com>
2015-03-26 14:59:15 +0600

    Wrong indentatiton.

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
 				dist: {
 					options: {
 						style: 'expanded',
-                        'sourcemap=none': true
+						'sourcemap=none': true
 					},
 					files: [ {
 						expand: true,
